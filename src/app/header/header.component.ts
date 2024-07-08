2d5adf39c4ce863076446b32e327990fc4f24699
Luiz Fehlebrg <84464998+20347-LuizF@users.noreply.github.com>
2024-07-08 18:12:58 +0100

    module-fouv

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,18 +3,22 @@ import { Category } from '../models/pie';
 import { PieService } from '../services/pie.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
+import { ROUTER_TOKENS } from '../app.routes';
 
 @Component({
   standalone: true,
   imports: [
     MatMenuModule,
     MatButtonModule,
+    RouterLink,
   ],
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  readonly ROUTER_TOKENS = ROUTER_TOKENS;
   readonly Category = Category;
   constructor(
     private readonly pieService: PieService
